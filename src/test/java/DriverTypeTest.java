04eda2779ba3f8209ea77155eb9e2dc31d2ed577
siraxle <sir.axle@yandex.ru>
2020-07-03 21:02:21 +0300

    recall mhetod

--- src/test/java/DriverTypeTest.java
+++ src/test/java/DriverTypeTest.java
@@ -3,13 +3,13 @@ import org.openqa.selenium.WebDriver;
 public class DriverTypeTest {
 
   public static void main(String[] args) {
-    DriverFactory webDriverFactory = createDeveloperByName("firefox");
+    DriverFactory webDriverFactory = createDriverByName("firefox");
     Driver driver = webDriverFactory.createDriver();
     WebDriver webDriver = driver.createDriver();
     webDriver.get("https://ya.ru");
   }
 
-  static DriverFactory createDeveloperByName(String brouserName) {
+  static DriverFactory createDriverByName(String brouserName) {
     if (brouserName.equalsIgnoreCase("chrome")) {
       return new DriverChromeFactory();
     } else if (brouserName.equalsIgnoreCase("fireFox")){
